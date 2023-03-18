d7668f5988c8ef49acd152ff99a19f788cc6aff3
sepowell <seanp1998@gmail.com>
2023-03-18 14:47:16 -0700

    Add grid sizing option
    Also re-organizes script layout to make more sense logically. groups together like items.

--- scripts.js
+++ scripts.js
@@ -1,73 +1,86 @@
 // Establish grid container
 const gridContainer = document.createElement("div");
-gridContainer.classList.add("normal-grid-container");
 document.body.appendChild(gridContainer);
 
-function createNormalGrid() {
-  for (let i = 0; i < 256; i++) {
-    const createDiv = document.createElement("div")
-    createDiv.classList.add("inner-div");
-    gridContainer.appendChild(createDiv);
-  }
-}
-
-// Loads grid into grid container on page load
+// Load grid into grid container on page load
 document.body.onload = createNormalGrid();
 
-// Links users choice buttons
-const normalChoice = document.getElementById("normal");
-const psychadelicChoice = document.getElementById("psychadelic");
+// Retrieve erase grid button
+const eraseGrid = document.getElementById("erase");
+eraseGrid.addEventListener("click", erase);
+
+// Retrieve color choice buttons
 var userChoice = "normal";
+const normalChoice = document.getElementById("normal");
 normalChoice.addEventListener("click", () => {
   userChoice = "normal";
-  console.log(userChoice);
 })
+const psychadelicChoice = document.getElementById("psychadelic");
 psychadelicChoice.addEventListener("click", () => {
   userChoice = "psychadelic";
-  console.log(userChoice);
 })
 
-// // Changes grid based on user choice
-// const gridChoiceNormal = document.getElementById("normal-grid");
-// const gridChoiceBig = document.getElementById("big-grid");
-// var gridChoice = "";
-// gridChoiceNormal.addEventListener("click", () => {
-//   gridChoice = "normal";
-//   changeGrid();
-// });
-// gridChoiceBig.addEventListener("click", () => {
-//   gridChoice = "big";
-//   changeGrid();
-// });
-
-// function changeGrid() {
-//   document.querySelectorAll(".inner-div").forEach(item => {
-//     item.remove();
-//   });
-//   if (gridChoice === "normal") {
-//     createNormalGrid();
-//   } else {
-//     createBigGrid();
-//   }
-// }
+// Retrieve grid choice buttons
+var gridChoice = "";
+const gridChoiceNormal = document.getElementById("normal-grid");
+gridChoiceNormal.addEventListener("click", () => {
+  gridChoice = "normal";
+  changeGrid();
+});
+const gridChoiceBig = document.getElementById("big-grid");
+gridChoiceBig.addEventListener("click", () => {
+  gridChoice = "big";
+  changeGrid();
+});
 
 // Recognizes mouseover event on all inner div's and colors
 // div based on user choice
-document.querySelectorAll(".inner-div").forEach(item => {
-  item.addEventListener("mouseover", (e) => {
-    if (userChoice === "normal") {
-      e.target.style.backgroundColor = '#707070';
-    } else {
-      const psychadelicPallete = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-      const randomColor = Math.floor(Math.random() * psychadelicPallete.length);
-      e.target.style.backgroundColor = psychadelicPallete[randomColor];
-    }
+function changeColor() {
+  document.querySelectorAll(".inner-div").forEach(item => {
+    item.addEventListener("mouseover", (e) => {
+      if (userChoice === "normal") {
+        e.target.style.backgroundColor = '#707070';
+      } else {
+        const psychadelicPallete = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+        const randomColor = Math.floor(Math.random() * psychadelicPallete.length);
+        e.target.style.backgroundColor = psychadelicPallete[randomColor];
+      }
+    });
   });
-});
+}
 
-// Erases grid
-const eraseGrid = document.getElementById("erase");
-eraseGrid.addEventListener("click", erase);
+function createNormalGrid() {
+  gridContainer.classList.add("normal-grid-container");
+  for (let i = 0; i < 256; i++) {
+    const createDiv = document.createElement("div");
+    createDiv.classList.add("inner-div");
+    gridContainer.appendChild(createDiv);
+  }
+  changeColor();
+}
+
+function createBigGrid() {
+  gridContainer.classList.add("big-grid-container");
+  for (let i = 0; i < 1024; i++) {
+    const createDiv = document.createElement("div");
+    createDiv.classList.add("inner-div");
+    gridContainer.appendChild(createDiv);
+  }
+  changeColor();
+}
+
+function changeGrid() {
+  const divs = document.querySelectorAll(".inner-div");
+  divs.forEach(item => {
+    item.remove();
+  });
+  gridContainer.className = " ";
+  if (gridChoice === "normal") {
+    createNormalGrid();
+  } else {
+    createBigGrid();
+  }
+}
 
 function erase() {
   document.querySelectorAll(".inner-div").forEach(item => {
